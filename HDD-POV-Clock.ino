204afb06d5c849fb9f5cf46c5ba7360fdcdbf0b5
Marco Wagner <marco1601@web.de>
2016-04-19 21:12:22 +0200

    ESC startup-calibration implemented

--- HDD-POV-Clock.ino
+++ HDD-POV-Clock.ino
@@ -6,12 +6,45 @@
 
 // CURRENTLY BEING TESTED: ESC control for HDD BLDC-motor!!
 
-void setup() {
-  // put your setup code here, to run once:
+Servo esc;  // create servo object to control a servo
 
+void setup() {
+  esc.attach(9);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(1000);
 
+  escCalibration();
+
+  esc.write(90);
+
+  delay(1000);
+
+  esc.write(150);
+
+  delay(1000);
+
+  esc.write(25);
+
+  while(1);
+
+  
 }
+
+void escCalibration() {
+  for(uint16_t i = 20; i<= 180 ; i++)
+  {
+    esc.write(i);
+    delay(5);
+  }
+  delay(100);
+  for(uint16_t i = 179; i>= 20; i--)
+  {
+    esc.write(i);
+    delay(5);
+  }
+
+  delay(5000);
+}
+
