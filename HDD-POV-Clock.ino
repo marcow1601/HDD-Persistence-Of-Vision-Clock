f1f5d32b3a6752e0b55a1c225a89bbfc31cbcc06
Marco Wagner <marco1601@web.de>
2016-04-19 19:21:46 +0200

    Initial Arduino project creation

---
+++ HDD-POV-Clock.ino
@@ -0,0 +1,17 @@
+// Main HDD POV Clock arduino code
+
+#include <Servo.h>
+
+// CURRENT STATUS: Only testing of single elements later needed
+
+// CURRENTLY BEING TESTED: ESC control for HDD BLDC-motor!!
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
