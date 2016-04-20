223222a20f804835996de84164b70d0aadbbe89a
Marco <marco1601@web.de>
2016-04-20 08:43:22 +0200

    Options for external interrupt added

--- HDD-POV-Clock.ino
+++ HDD-POV-Clock.ino
@@ -1,6 +1,8 @@
 // Main HDD POV Clock arduino code
 
-#include <Servo.h>
+#include <Servo.h>            // Library to output 1-2ms pulses @ 50Hz
+#include <avr/io.h>         // Definition of interrupt names
+#include <avr/interrupt.h>  // ISR interrupt service routine
 
 // CURRENT STATUS: Only testing of single elements later needed
 
@@ -8,43 +10,56 @@
 
 Servo esc;  // create servo object to control a servo
 
+// This is the INT0 Pin of the ATMega8
+int hallSensor = 2;
+// INT data exchange variables
+volatile uint16_t revTime = 0;
+volatile uint16_t lastRev = micros();
+
+// Install the interrupt routine.
+//ISR(INT0_vect) {
+//revTime = micros();
+//}
+
 void setup() {
   esc.attach(9);  // attaches the servo on pin 9 to the servo object
+
+  // https://gonium.net/md/2006/12/20/handling-external-interrupts-with-arduino/
+  //pinMode(hallSensor, INPUT);
+  // Global Enable INT0 interrupt
+  //GICR |= ( 1 < < INT0);
+  // Signal change triggers interrupt
+  //MCUCR |= ( 1 << ISC00);
+  //MCUCR |= ( 0 << ISC01);
+
+  //http://fluuux.de/2013/04/interrupts-mit-arduino-benutzen/
+  attachInterrupt(0, hallISR, FALLING);
 }
 
 void loop() {
-  delay(1000);
-
   escCalibration();
-
-  esc.write(90);
-
-  delay(1000);
-
-  esc.write(150);
-
-  delay(1000);
-
-  esc.write(25);
-
-  while(1);
 
   
 }
 
 void escCalibration() {
-  for(uint16_t i = 20; i<= 180 ; i++)
+  // Sweep ESC control one cycle Min-Max-Min on power-up
+  for(uint8_t i = 20; i<= 180 ; i++)
   {
     esc.write(i);
     delay(5);
   }
   delay(100);
-  for(uint16_t i = 179; i>= 20; i--)
+  for(uint8_t i = 179; i>= 20; i--)
   {
     esc.write(i);
     delay(5);
   }
-
+  // Wait for initialization sound to finish
   delay(5000);
 }
 
+void hallISR() {
+  revTime = micros() - lastRev;
+  lastRev = micros();
+}
