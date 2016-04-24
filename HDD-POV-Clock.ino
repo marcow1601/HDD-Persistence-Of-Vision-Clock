e91e5a7f8ed520bf0e155eae6be2df584bc5c0bc
Marco Wagner <marco1601@web.de>
2016-04-24 16:57:35 +0200

    First segmenting tests without sensors

--- HDD-POV-Clock.ino
+++ HDD-POV-Clock.ino
@@ -1,44 +1,88 @@
-// Main HDD POV Clock arduino code
+/****************************************************************
+ * 
+ * Hard drive disk (HDD) Persistence-of-Vision (POV) Clock 
+ * 
+ * Marco Wagner
+ * April 2016
+ * 
+******************************************************************/
 
-#include <Servo.h>            // Library to output 1-2ms pulses @ 50Hz
-#include <avr/io.h>         // Definition of interrupt names
-#include <avr/interrupt.h>  // ISR interrupt service routine
+#include <Servo.h>           // Library to output 1-2ms pulses @ 50Hz
+#include <avr/io.h>          // Definition of interrupt names
+#include <avr/interrupt.h>   // ISR interrupt service routine
+#include <TimerOne.h>        // Arduino Timer 1 library
 
-// CURRENT STATUS: Only testing of single elements later needed
+#define PIN_R       3
+#define PIN_G       5
+#define PIN_B       6
+#define PIN_HALL    2       // This is the INT0 Pin of the ATMega8
 
-// CURRENTLY BEING TESTED: ESC control for HDD BLDC-motor!!
+struct LED
+{
+  bool red : 1;
+  bool green : 1;
+  bool blue : 1;  
+};
 
 Servo esc;  // create servo object to control a servo
 
-// This is the INT0 Pin of the ATMega8
-int hallSensor = 2;
-// INT data exchange variables
+//############################
+//####### Variables ##########
+//############################
+
+LED segment[256] = {0};
+
+// Global, interrupt accessible variables
 volatile uint16_t revTime = 0;
 volatile uint16_t lastRev = micros();
+volatile uint16_t segTime = 0;
 
-// Install the interrupt routine.
-//ISR(INT0_vect) {
-//revTime = micros();
-//}
+uint8_t pin = PIN_R;
+
 
 void setup() {
+  pinMode(PIN_HALL, INPUT);
+  
+  pinMode(PIN_R, OUTPUT);
+  pinMode(PIN_G, OUTPUT);
+  pinMode(PIN_B, OUTPUT);
+
+  digitalWrite(PIN_R, LOW);
+  digitalWrite(PIN_G, LOW);
+  digitalWrite(PIN_B, LOW);
+  
   esc.attach(9);  // attaches the servo on pin 9 to the servo object
 
-  // https://gonium.net/md/2006/12/20/handling-external-interrupts-with-arduino/
-  //pinMode(hallSensor, INPUT);
-  // Global Enable INT0 interrupt
-  //GICR |= ( 1 < < INT0);
-  // Signal change triggers interrupt
-  //MCUCR |= ( 1 << ISC00);
-  //MCUCR |= ( 0 << ISC01);
+  Timer1.initialize(4190);
+  Timer1.attachInterrupt(blinken);
 
   //http://fluuux.de/2013/04/interrupts-mit-arduino-benutzen/
-  attachInterrupt(0, hallISR, FALLING);
+  attachInterrupt(digitalPinToInterrupt(PIN_HALL), hallISR, FALLING);
 }
 
 void loop() {
-  escCalibration();
+  //escCalibration();
 
+  for(uint8_t i = 0; i<3; i++)
+  {
+   if(i==0){
+    pin = PIN_R;
+    digitalWrite(PIN_G, LOW);
+    digitalWrite(PIN_B, LOW);
+   }
+   else if(i==1){
+    pin = PIN_G;
+    digitalWrite(PIN_R, LOW);
+    digitalWrite(PIN_B, LOW);
+   }
+   else{
+    pin = PIN_B;
+    digitalWrite(PIN_R, LOW);
+    digitalWrite(PIN_G, LOW);
+   }
+
+    delay(2000);
+  }
   
 }
 
@@ -62,4 +106,12 @@ void escCalibration() {
 void hallISR() {
   revTime = micros() - lastRev;
   lastRev = micros();
+  segTime = (int) revTime / 256.0f;
+
+  Timer1.setPeriod(segTime);
 }
+
+void blinken() {
+  digitalWrite(pin, digitalRead(pin)^1);
+}
+
