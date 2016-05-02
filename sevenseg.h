48b4822f0a3b759855fe5d047dfff10338e3dad9
Marco <marco1601@web.de>
2016-05-02 12:54:59 +0200

    Update sevenseg.h

--- sevenseg.h
+++ sevenseg.h
@@ -8,9 +8,11 @@
 #endif
 
 
+void init7seg(uint8_t d1, uint8_t d2, uint8_t d3, uint8_t d4, uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint8_t e, uint8_t f, uint8_t g, uint8_t dp, uint8_t colAn, uint8_t colCat);
 
-void setOutput(uint16_t value);   //
-void setMode(boolean colon);      //
+void setMode(boolean colon);
+void setOutput(uint8_t hour, uint8_t minute);
+
 void multiplex();
 void enable7seg();
 void disable7seg();
@@ -18,4 +20,3 @@ void disable7seg();
 
 
 #endif
-
